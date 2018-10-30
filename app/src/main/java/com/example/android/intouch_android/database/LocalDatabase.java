0056c39405927071420415972bab8e7fd1e884fb
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-10-30 01:24:24 -0400

    Update version so letters in DB fetched new version from fake server w/ recipientId

--- app/src/main/java/com/example/android/intouch_android/database/LocalDatabase.java
+++ app/src/main/java/com/example/android/intouch_android/database/LocalDatabase.java
@@ -13,7 +13,7 @@ import com.example.android.intouch_android.model.Letter;
 
 @Database(
         entities = {Letter.class, Inmate.class, Correspondence.class},
-        version = 5,
+        version = 6,
         exportSchema = false
 )
 @TypeConverters(DateConverter.class)
