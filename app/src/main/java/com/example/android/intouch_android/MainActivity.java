793f5c86be301bd6bbe693708a387f9da7bcaf7e
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-10-31 02:10:00 -0400

    Remove dead code from MainActivity

--- app/src/main/java/com/example/android/intouch_android/MainActivity.java
+++ app/src/main/java/com/example/android/intouch_android/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.intouch_android;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,11 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
-
-import com.example.android.intouch_android.ui.inmatesearch.InmateSearchFragment;
-import com.example.android.intouch_android.ui.lettereditor.LetterEditorFragment;
-import com.example.android.intouch_android.ui.sentletters.SentLettersFragment;
-import com.example.android.intouch_android.model.Letter;
 
 import androidx.navigation.Navigation;
 
@@ -33,25 +27,14 @@ public class MainActivity
 
     }
 
-    // TODO: Android sets up this boilerplate but why do our fragments need to talk to the activity?
-    public void onListFragmentInteraction(Letter item) {
-        System.out.println(item.toString());
-    }
-    public void onFragmentInteraction(Uri uri) {
-        System.out.println(uri.toString());
-    }
-
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.navigation_home:
-                mTextMessage.setText(R.string.title_home);
                 return true;
             case R.id.navigation_dashboard:
-                mTextMessage.setText(R.string.title_dashboard);
                 return true;
             case R.id.navigation_notifications:
-                mTextMessage.setText(R.string.title_notifications);
                 return true;
         }
         return false;
