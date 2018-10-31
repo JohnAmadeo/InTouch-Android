500bd2b3e26c88295a5b2b3d456dd99712b44430
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-10-31 02:06:50 -0400

    Implement auth flow for login/signup using Auth0

---
+++ app/src/main/java/com/example/android/intouch_android/utils/AuthUtils.java
@@ -0,0 +1,42 @@
+package com.example.android.intouch_android.utils;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.jwt.JWT;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
+import com.example.android.intouch_android.R;
+import com.example.android.intouch_android.model.User;
+
+public class AuthUtils {
+    private String LOG_TAG = this.getClass().getSimpleName();
+
+    public static void login(Activity activity, AuthCallback authCallback) {
+        Auth0 account = new Auth0(activity.getApplicationContext());
+        account.setOIDCConformant(true);
+
+        WebAuthProvider.init(account)
+                .withScheme("demo")
+                .withScope("openid profile email")
+                .withAudience("https://intouch-android-backend.herokuapp.com/")
+                .start(activity, authCallback);
+    }
+
+    public static User getUserFromCredentials(Credentials credentials) {
+        JWT idToken = new JWT(credentials.getIdToken());
+        String username = idToken.getClaim("nickname").asString();
+        String email = idToken.getClaim("name").asString();
+
+        return new User(
+                username,
+                email,
+                credentials.getAccessToken()
+        );
+    }
+}
