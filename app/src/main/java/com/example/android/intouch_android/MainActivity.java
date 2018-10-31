054b3499c53945f72e27c099bd8f85f06aef8b93
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-10-31 14:40:13 -0400

    Remove dead code in MainActivity

--- app/src/main/java/com/example/android/intouch_android/MainActivity.java
+++ app/src/main/java/com/example/android/intouch_android/MainActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import androidx.navigation.Navigation;
 
@@ -15,7 +13,6 @@ public class MainActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private final String LOG_TAG = this.getClass().getSimpleName();
-    private TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
