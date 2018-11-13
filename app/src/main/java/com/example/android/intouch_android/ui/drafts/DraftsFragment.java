67d0d5f4388d9cbdf3ac43414b07be3c5c74e07c
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-11-13 11:30:36 -0500

    Implement list of drafts
    A lot of the logic is duplicated from SentLetters, so we should refactor out stuff later on for DRY purposes

--- app/src/main/java/com/example/android/intouch_android/ui/drafts/DraftsFragment.java
+++ app/src/main/java/com/example/android/intouch_android/ui/drafts/DraftsFragment.java
@@ -1,9 +1,11 @@
 package com.example.android.intouch_android.ui.drafts;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.util.Pair;
 import android.view.LayoutInflater;
@@ -14,16 +16,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android.intouch_android.R;
+import com.example.android.intouch_android.common.LettersAdapter;
 import com.example.android.intouch_android.utils.ViewUtils;
+import com.example.android.intouch_android.viewmodel.DraftsViewModel;
 
 import java.util.Arrays;
 import java.util.List;
 
-import androidx.navigation.Navigation;
-
 public class DraftsFragment extends Fragment {
+    private final String LOG_TAG = this.getClass().getSimpleName();
     private List<Integer> VISIBLE_MENU_ITEMS = Arrays.asList(R.id.menu_search);
+
     private View mParentView;
+    private RecyclerView mRecyclerView;
+    private MenuItem mSearchMenuItem;
+    private SearchView mSearchView;
+
+    private DraftsViewModel mViewModel;
 
     public DraftsFragment() { }
 
@@ -42,6 +51,7 @@ public class DraftsFragment extends Fragment {
 
         mParentView = inflater.inflate(R.layout.fragment_drafts, container, false);
 
+        setupRecyclerView();
         ViewUtils.setupBottomNavigation(
                 getActivity(),
                 mParentView,
@@ -49,6 +59,11 @@ public class DraftsFragment extends Fragment {
                 R.id.draftsFragment,
                 () -> { mSearchMenuItem.collapseActionView(); }
         );
+
+        mViewModel = ViewModelProviders.of(this).get(DraftsViewModel.class);
+        mViewModel.getDisplayedDrafts().observe(this, drafts -> {
+            getRecyclerViewAdapter().setLetters(drafts);
+        });
 
         return mParentView;
     }
@@ -59,8 +74,73 @@ public class DraftsFragment extends Fragment {
         inflater.inflate(R.menu.options_menu, menu);
 
         ViewUtils.setupMenuItems(menu, VISIBLE_MENU_ITEMS);
+
+        Pair<MenuItem, SearchView> views = ViewUtils.setupSearch(
+                menu,
+                R.string.drafts_search_hint,
+                getActivity()
+        );
+
+        mSearchMenuItem = views.first;
+        mSearchView = views.second;
+
+        mSearchView.setOnQueryTextListener(createQueryListener());
     }
 
     @Override
-    public void onDetach() { super.onDetach(); }
+    public void onDetach() {
+        super.onDetach();
+        getActivity().invalidateOptionsMenu();
+    }
+
+    /* ************************************************************ */
+    /*                            Setup Helpers                     */
+    /* ************************************************************ */
+    private void setupRecyclerView() {
+        mRecyclerView = mParentView.findViewById(R.id.fragment_drafts);
+        Context context = mRecyclerView.getContext();
+        LettersAdapter recyclerViewAdapter = new LettersAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+
+        // set layout of RecyclerView to a list of vertically scrolling items
+        mRecyclerView.setLayoutManager(layoutManager);
+        // specify what each list item looks like
+        mRecyclerView.setAdapter(recyclerViewAdapter);
+
+        // add a line in between each list item
+        mRecyclerView.addItemDecoration(
+                ViewUtils.createListDivider(
+                        mRecyclerView,
+                        (LinearLayoutManager) mRecyclerView.getLayoutManager()
+                )
+        );
+    }
+
+    /* ************************************************************ */
+    /*                            Listeners                         */
+    /* ************************************************************ */
+    private SearchView.OnQueryTextListener createQueryListener() {
+        return new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String searchQuery) {
+                mViewModel.setQuery(searchQuery);
+                mRecyclerView.scrollToPosition(0);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String searchQuery) {
+                ViewUtils.dismissKeyboard(getActivity());
+                return true;
+            }
+        };
+    }
+
+    /* ************************************************************ */
+    /*                              Helpers                         */
+    /* ************************************************************ */
+
+    private LettersAdapter getRecyclerViewAdapter() {
+        return (LettersAdapter) mRecyclerView.getAdapter();
+    }
 }
---
+++ app/src/main/java/com/example/android/intouch_android/viewmodel/DraftsViewModel.java
@@ -0,0 +1,85 @@
+package com.example.android.intouch_android.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Transformations;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.android.intouch_android.model.Letter;
+import com.example.android.intouch_android.model.container.Resource;
+import com.example.android.intouch_android.repository.LettersRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DraftsViewModel extends AndroidViewModel {
+    private final String LOG_TAG = this.getClass().getSimpleName();
+
+    private LettersRepository mLettersRepository;
+    private MutableLiveData<Boolean> mRefreshRequest = new MutableLiveData<>();
+    private MutableLiveData<String> mSearchQuery = new MutableLiveData<>();
+
+    private LiveData<Resource<List<Letter>>> mDrafts;
+    private MediatorLiveData<List<Letter>> mDisplayedDrafts = new MediatorLiveData<>();
+
+    public DraftsViewModel(@NonNull Application application) {
+        super(application);
+        mLettersRepository = new LettersRepository(application.getApplicationContext());
+
+        // "Start" the streams with an initial value. Libraries like RxJava have a .startWith()
+        // function that automatically does this. Probably can extend LiveData ourselves to do this
+        // but pretty overkill at this stage of the project.
+        mRefreshRequest.setValue(true);
+        mSearchQuery.setValue("");
+
+        mDrafts = Transformations.switchMap(mRefreshRequest,
+                __ -> mLettersRepository.getDrafts()
+        );
+
+        mDisplayedDrafts.addSource(mSearchQuery, searchQuery -> {
+            Log.d(LOG_TAG, "Search query: " + searchQuery);
+            mDisplayedDrafts.setValue(filterLetters(mDrafts.getValue(), searchQuery));
+        });
+
+        mDisplayedDrafts.addSource(mDrafts, letters -> {
+
+            mDisplayedDrafts.setValue(filterLetters(letters, mSearchQuery.getValue()));
+        });
+    }
+
+    public LiveData<Resource<List<Letter>>> getDrafts() {
+        return mDrafts;
+    }
+    public LiveData<List<Letter>> getDisplayedDrafts() { return mDisplayedDrafts; }
+
+    public void setRefreshRequest() { mRefreshRequest.setValue(true); }
+    public void setQuery(String searchQuery) { mSearchQuery.setValue(searchQuery); }
+
+    /* ************************************************************ */
+    /*                     Private Functions                        */
+    /* ************************************************************ */
+
+    private List<Letter> filterLetters(
+            Resource<List<Letter>> letters,
+            String searchQuery
+    ) {
+        if (letters == null || letters.data == null) {
+            return new ArrayList<>();
+        }
+        if (searchQuery == null) {
+            return letters.data;
+        }
+
+        List<Letter> filteredLetters = new ArrayList<>();
+        for (Letter letter:letters.data) {
+            if (letter.contains(searchQuery)) {
+                filteredLetters.add(letter);
+            }
+        }
+        return filteredLetters;
+    }
+}
